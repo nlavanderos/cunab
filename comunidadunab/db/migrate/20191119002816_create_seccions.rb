2945df64020895c1bdd51354d38cbe47d96d5700
root <root@localhost.localdomain>
2019-11-20 13:35:46 -0300

    fixes

--- comunidadunab/db/migrate/20191119002816_create_seccions.rb
+++ comunidadunab/db/migrate/20191119002816_create_seccions.rb
@@ -1,6 +1,6 @@
 class CreateSeccions < ActiveRecord::Migration[6.0]
   def change
-    create_table :seccions  do |t|
+    create_table :seccions do |t|
       t.integer :id
       t.integer :seccion
 
